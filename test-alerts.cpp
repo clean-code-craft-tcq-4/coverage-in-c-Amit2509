e8d4bccdf9e72505f841f9a796350e0791b5f5e6
Amit2509 <amitsrivastava2509@gmail.com>
2022-08-18 02:09:46 +0530

    Update test-alerts.cpp
    adding more test cases

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,4 +5,13 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(40, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+}
+
+TEST_CASE("classify the breach according to type of cooling and temperature") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 42) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 42) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 42) == NORMAL);
+   
 }
