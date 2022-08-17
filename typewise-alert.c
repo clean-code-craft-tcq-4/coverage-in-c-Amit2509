176038e5b3ad034c1a0640fe5a304370b26a4c2d
Amit2509 <amitsrivastava2509@gmail.com>
2022-08-18 01:41:50 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -65,7 +65,6 @@ void sendToEmail(BreachType breachType) {
       printf("To: %s\n", recepient);
       printf("Hi, the temperature is too high\n");
       break;
-    case NORMAL:
     default:
       break;
   }
